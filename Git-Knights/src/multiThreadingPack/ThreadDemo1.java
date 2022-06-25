c59780ab24c769421a0ef9778ff6e146ce478e76
HP <HP@DESKTOP-N078L4C>
2022-06-25 12:30:03 +0530

    Thread demo

---
+++ Git-Knights/src/multiThreadingPack/ThreadDemo1.java
@@ -0,0 +1,28 @@
+package multiThreadingPack;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadDemo1 {
+	public static void main(String[] args)throws Exception {
+		ExecutorService es=Executors.newFixedThreadPool(1);
+		es.execute(new MyThreadJob());
+		
+		//Thread t=Thread.currentThread();
+		//System.out.println(t);
+		for(int i=0;i<5;i++) {			
+			System.out.println(i);
+			Thread.sleep(1000);
+		}
+		es.shutdown();
+	}
+}
+
+//job of thread
+
+class MyThreadJob implements Runnable{
+	@Override
+	public void run() {
+		System.out.println("child thread logic goes here...");
+	}
+}
